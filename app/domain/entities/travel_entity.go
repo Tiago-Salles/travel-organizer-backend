3f70baf7003e63eed12ef0d88c3e90111734a7d6
Tiago <tiago.salles@houseasy.net>
2022-08-05 07:33:16 -0300

    Connected to mongo DB through docker

---
+++ app/domain/entities/travel_entity.go
@@ -0,0 +1,6 @@
+package main
+
+type TravelModel struct {
+	Country string `json:"country"`
+	City    string `json:"city"`
+}
--- app/main.go
+++ app/main.go
@@ -2,33 +2,25 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
-
-func startDataBase() {
-	context, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	client, err := mongo.Connect(context, options.Client().ApplyURI("mongodb://localhost:27017"))
-	if err = client.Disconnect(context); err != nil {
-		panic(err)
-	}
-}
 
 type TravelModel struct {
 	Country string `json:"country"`
 	City    string `json:"city"`
 }
 
-func generateTravel() []TravelModel {
-	var travel = []TravelModel{
-		{Country: "Portugal", City: "Lisbon"},
-		{Country: "Spain", City: "Madrid"},
-		{Country: "Spain", City: "Barcelona"},
+func generateTravel() TravelModel {
+	var travel = TravelModel{
+		Country: "Portugal",
+		City:    "Lisbon",
 	}
 	return travel
 }
@@ -37,13 +29,29 @@ func fetchTravel(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, generateTravel())
 }
 
-func runApp() {
+func runApp(data any) {
 	router := gin.Default()
 	router.GET("/travel", fetchTravel)
 	router.Run("localhost:4040")
 }
 
 func main() {
-	defer startDataBase()
-	runApp()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, e := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if e != nil {
+		client.Disconnect(ctx)
+		panic(e)
+	}
+	collection := client.Database("travels").Collection("travel")
+	result, err := collection.InsertOne(context.Background(), generateTravel())
+	if err != nil {
+		panic(err)
+	}
+	dataFromDb, _e := collection.Find(ctx, bson.M{})
+	if _e != nil {
+		panic(e)
+	}
+	fmt.Print(result.InsertedID)
+	runApp(dataFromDb)
 }
