56b933ccc5dc8cd174a8da5be1d0c9422f1a8289
Tiago <tiago.salles@houseasy.net>
2022-08-08 07:46:07 -0300

    Adjusted packages import

--- app/main.go
+++ app/main.go
@@ -1,32 +1,9 @@
 package main
-package GenerateTravel
-
 
 import (
-	"context"
-	"fmt"
-	"net/http"
-	"time"
-
-	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	useCase "travel_organizer_backend/app/domain/use_cases"
 )
 
-
-
-func fetchTravel(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, GenerateTravel())
-}
-
-func runApp(data any) {
-	router := gin.Default()
-	router.GET("/travel", fetchTravel)
-	router.Run("localhost:4040")
-}
-
 func main() {
-	
-	runApp(dataFromDb)
+	useCase.RunApp()
 }
